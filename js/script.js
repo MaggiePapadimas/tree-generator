d381f5165998817a9708fc3eb55a1c0c63d1c601
MaggiePapadimas <maggie.papadimas@hotmail.com>
2019-04-21 22:07:26 -0400

    making sure three.js is working

--- js/script.js
+++ js/script.js
@@ -6,10 +6,26 @@ var renderer = new THREE.WebGLRenderer();
 
 renderer.setSize (window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+window.addEventListener('resize', function(){
+	var w = window.innerWidth;
+	var h = window.innerHeight;
+	renderer.setSize(w,h);
+	camera.aspect = w/h;
+	camera.updateProjectionMatrix();
 
+});
+
+
+var geom = new THREE.BoxGeometry(1,1,1);
+var mat = new THREE.MeshBasicMaterial( {color : 0xFFFFFF, wireframe : false});
+var cube = new THREE.Mesh(geom, mat);
+scene.add(cube);
+camera.position.z = 3;
 //logic
 var update = function(){
-
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.001;
+	cube.rotation.z += 0.0001;
 };
 // draws scene
 var render = function(){
